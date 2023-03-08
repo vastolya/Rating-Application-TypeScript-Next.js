0d23a110c5ce2cfd180d874ae0bffb75f764ed1f
paramee@bk.ru <paramee@bk.ru>
2023-03-08 23:25:19 +0700

    tag & paragraph components added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P, Tag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" className="asdasd">Button</Button>
+      <Button appearance="primary" arrow='right'>Button</Button>
       <Button appearance="ghost">Button</Button>
+      <P size='l'>BIG ONE</P> 
+      <P >MEDIUM TWO</P> 
+      <Tag size='s'>ghost</Tag>
+      <Tag size='m' color='red'>red</Tag>
+      <Tag size='s' color='green'>green</Tag>
+      <Tag color='primary'>primary</Tag>
     </div>
   );
 }
