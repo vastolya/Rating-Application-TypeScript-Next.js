1ebd85d257278baf0f181744c462b16512b2b242
paramee@bk.ru <paramee@bk.ru>
2023-03-10 22:48:49 +0700

    Layout HOC added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,44 @@
-import { Button, Htag, P, Tag } from "../components";
+import { withLayout } from "@/layout/Layout";
+import { useEffect, useState } from "react";
+import { Button, Htag, P, Tag, Rating } from "../components";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  const [rating, setRating] = useState<number>(4);
+
+  useEffect(() => {
+    console.log("counter " + counter);
+    return function cleanUp() {
+      console.log("unmount");
+    };
+  });
+
   return (
-    <div>
-      <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow='right'>Button</Button>
+    <>
+      <Htag tag="h1">{counter}</Htag>
+      <Button
+        appearance="primary"
+        arrow="right"
+        onClick={() => setCounter((x) => x + 1)}
+      >
+        plus me
+      </Button>
       <Button appearance="ghost">Button</Button>
-      <P size='l'>BIG ONE</P> 
-      <P >MEDIUM TWO</P> 
-      <P size='s'>SMALL THREE</P> 
-      <Tag size='s'>ghost</Tag>
-      <Tag size='m' color='red'>red</Tag>
-      <Tag size='s' color='green'>green</Tag>
-      <Tag color='primary'>primary</Tag>
-    </div>
+      <P size="l">BIG ONE</P>
+      <P>MEDIUM TWO</P>
+      <P size="s">SMALL THREE</P>
+      <Tag size="s">ghost</Tag>
+      <Tag size="m" color="red">
+        red
+      </Tag>
+      <Tag size="s" color="green">
+        green
+      </Tag>
+      <Tag color="primary">primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+    </>
   );
 }
+
+export default withLayout(Home);
